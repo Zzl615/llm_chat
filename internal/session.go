5955da8bc1222a7687b13f6d5bd92515e1d33498
zhangzongliang <noaghzil@gmail.com>
2025-11-09 14:04:39 +0800

    refactor: 将 Manager 拆分到独立的 manager.go 文件

--- internal/session.go
+++ internal/session.go
@@ -8,7 +8,6 @@ package internal
 
 import (
 	"log"
-	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -35,35 +34,6 @@ func NewSession(id string, conn *websocket.Conn) *Session {
 		Send:    make(chan []byte, 128),
 		CloseCh: make(chan struct{}),
 	}
-}
-
-// Manager 维护所有活跃 session
-type Manager struct {
-	mu       sync.RWMutex
-	sessions map[string]*Session
-}
-
-func NewManager() *Manager {
-	return &Manager{sessions: make(map[string]*Session)}
-}
-
-func (m *Manager) Register(s *Session) {
-	m.mu.Lock()
-	m.sessions[s.ID] = s
-	m.mu.Unlock()
-}
-
-func (m *Manager) Unregister(id string) {
-	m.mu.Lock()
-	delete(m.sessions, id)
-	m.mu.Unlock()
-}
-
-func (m *Manager) Get(id string) (*Session, bool) {
-	m.mu.RLock()
-	s, ok := m.sessions[id]
-	m.mu.RUnlock()
-	return s, ok
 }
 
 // WebSocket读协程
